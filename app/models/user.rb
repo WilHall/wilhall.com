ed4f5786ae995029e8a564e50c095cc21e237168
Wil Hall <wil@wilhall.com>
2024-08-30 16:42:46 -0400

    Add analytics to admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :lockable #, :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable, :lockable # , :registerable
 end
